66747d2c814a1cf23ae98a39cf17b041fde613a2
Steven Nguyen <stnguyen90@gmail.com>
2012-06-15 18:20:22 -0700

    compile and run script for corner-detection now working

---
+++ Makefile
@@ -0,0 +1,12 @@
+all:
+	#corner-detection
+	make --directory=corner-detection all
+
+	#rotation
+	make --directory=Rotation all
+
+	#scaling
+
+	#gaussian-blur
+
+	#high-pass-filter
---
+++ cs133-img-proc.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+	echo -e "Usage:"
+	echo -e 	"\t$0 img-op img-op-args";
+	echo -e "Where";
+	echo -e 	"\timg-op = { motion-estimation[-parallel], corner-detection[-parallel], rotation[-parallel], scaling[-parallel], gaussian-blur[-parallel], high-pass-filter[-parallel] }"
+	echo -e "and img-op-args are the arguments for that operation.";
+
+	exit 0;
+
+else
+
+	if [ "$1" = "motion-estimation" ]; then
+		echo ${@:2};
+	elif [ "$1" = "corner-detection" ]; then
+		if [ $# -lt 3 ]; then
+			echo -e $1 "usage: ";
+			echo -e 	"\t$0 $1 input_file output_file [threshold]";
+
+			exit 0;
+		else
+			./corner-detection/bin/corner-detection-sequential "${@:2}";
+		fi
+	elif [ "$1" = "corner-detection-parallel" ]; then
+		if [ $# -lt 3 ]; then
+			echo -e $1 "usage: ";
+			echo -e 	"\t$0 $1 input_file output_file [threshold] [num_threads]";
+
+			exit 0;
+		else
+			./corner-detection/bin/corner-detection-parallel "${@:2}";
+		fi
+	elif [ "$1" = "rotation" ]; then
+		echo $@;
+	elif [ "$1" = "scaling" ]; then
+		echo $@;
+	elif [ "$1" = "gaussian-blur" ]; then
+		echo $@;
+	elif [ "$1" = "high-pass-filter" ]; then
+		echo $@;
+	fi
+fi
